d5403a3a6f5ea0a482d569f09f78d1ec64d96d35
Sebastian <thesicrs@gmail.com>
2020-08-02 18:58:57 +0700

    Use simd in the activation function
    A few WIP methods have been included in the commit

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,14 @@
 #![allow(dead_code, unused_imports, unused_variables)]
 extern crate nalgebra as na;
 mod vector;
+mod train;
+mod source;
 
+#[cfg(all(any(target_arch = "x86_64", target_arch = "x86"), target_feature = "simd"))]
+use faster::*;
 use na::{ Matrix, Dynamic, VecStorage };
 use self::vector::Vector;
+use self::source::DataSource;
 
 pub enum ActivationFunc {
     Sigmoid,
@@ -17,8 +22,31 @@ impl ActivationFunc {
             }
         })
     }
+
+    fn get_fn(self) -> Box<dyn Fn(Vector) -> Vector> {
+        Box::new(match self {
+            _ => |input: Vector| {
+                let inner: Vec<f64> = input.into();
+
+                #[cfg(all(any(target_arch = "x86_64", target_arch = "x86"), target_feature = "simd"))]
+                let res: Vec<f64> = inner
+                    .simd_iter(f64s(0.0))
+                    .simd_map(|x| f64s(1.0) / (f64s(1.0) + f64s(std::f64::consts::E).powf(- x)))
+                    .scalar_collect();
+
+                #[cfg(not(all(any(target_arch = "x86_64", target_arch = "x86"), target_feature = "simd")))]
+                let res: Vec<f64> = inner
+                    .iter()
+                    .map(|x| 1.0 / (1.0 + std::f64::consts::E.powf(- x)))
+                    .collect();
+
+                Vector::from(res)
+            }
+        })
+    }
 }
 
+/// The neural network
 pub struct Network {
     activation_function: Box<dyn Fn(f64) -> f64>,
     bias_matrix: Vec<Vec<f64>>,
@@ -31,7 +59,7 @@ impl Network {
         let n_layers: usize = configuration.len();
         let mut bias_matrix: Vec<Vec<f64>> = Vec::with_capacity(n_layers);
         let mut weight_matrix: Vec<Vec<Vector>> = Vec::with_capacity(n_layers);
-        // populate the bias and weight matrices
+        // populate the bias matrix
         for i in 0..bias_matrix.capacity() {
             // the matrix is a table with a bias for each node in the network
             bias_matrix[i] = [0.0].repeat(configuration[i]);
@@ -45,13 +73,31 @@ impl Network {
             }
         }
 
-        let netw = Network { 
-            configuration,
-            bias_matrix,
-            weight_matrix,
+        Network { 
             activation_function: activation_function.get(),
-        };
-        
-        netw
+            bias_matrix,
+            configuration,
+            weight_matrix,
+        }
+    }
+
+    /// Calculate output for the activation func input
+    pub fn feedforward(&mut self, layer: usize, neuron: usize, data: &Vector) -> f64 {
+        let weight_vec: &Vector = &self.weight_matrix[layer][neuron];
+        if data.len() != weight_vec.len() {
+            panic!("Vectors are not of the same dimension");
+        }
+
+        weight_vec * data + self.bias_matrix[layer][neuron]
+    }
+
+    pub fn train<M: train::TrainMethod, D: DataSource>(&mut self, method: M, training_data: D) {
+        let ref bias_ref = self.bias_matrix;
+        let ref weight_ref = self.weight_matrix;
+        if let self::source::DataKind::Training = training_data.kind() {
+            
+        } else {
+            panic!("Non-training data used");
+        }
     }
 }
