a84edc241033ab66883da2c7e5f0313e4665a954
Sebastian <thesicrs@gmail.com>
2020-08-09 21:20:07 +0700

    Move train.rs into mod.rs
    Signed-off-by: Sebastian <thesicrs@gmail.com>

---
+++ src/train/mod.rs
@@ -0,0 +1,11 @@
+pub mod sgd;
+
+use crate::{source::DataSource, ActivationFunction, Network, Vector};
+
+pub trait Trainer {
+    fn train<A: ActivationFunction, D: DataSource<(Vector, Vector)>>(
+        &mut self,
+        net: &mut Network<A>,
+        data: D,
+    );
+}
